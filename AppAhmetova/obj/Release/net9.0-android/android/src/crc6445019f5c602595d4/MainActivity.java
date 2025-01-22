51cc4c5c336571abfa001322989311f1cf271629
admnssen <admnssenn@gmail.com>
2025-01-22 12:15:42 +0300

    project files

---
+++ AppAhmetova/obj/Release/net9.0-android/android/src/crc6445019f5c602595d4/MainActivity.java
@@ -0,0 +1,46 @@
+package crc6445019f5c602595d4;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("AppAhmetova.MainActivity, AppAhmetova", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("AppAhmetova.MainActivity, AppAhmetova", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("AppAhmetova.MainActivity, AppAhmetova", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
